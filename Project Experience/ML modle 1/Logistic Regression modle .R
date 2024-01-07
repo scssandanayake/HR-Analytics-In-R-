6371623443db52c000a0e8f8a10cc24d9588c417
scssandanayake <114943338+scssandanayake@users.noreply.github.com>
2024-01-07 07:32:19 +0530

    ML modle 1 added

---
+++ Project Experience/ML modle 1/Logistic Regression modle .R
@@ -0,0 +1,68 @@
+library(caTools)
+library(ROCR)
+library(dplyr)
+
+dataset <- read.csv("D:\\N learn\\2nd year\\projects\\R project\\HR_Analytics.csv")
+summary(dataset)
+
+dataset <- dataset %>%
+  mutate(Attrition = ifelse(Attrition == "No",0,1))
+
+str(dataset)
+
+# Splitting dataset
+set.seed(123)
+split <- sample.split(dataset, SplitRatio = 0.7)
+split
+
+train_reg <- subset(dataset, split == "TRUE")
+test_reg <- subset(dataset, split == "FALSE")
+
+# Training model
+logistic_model <- glm(Attrition ~ JobLevel + TotalWorkingYears,
+                      data = train_reg,
+                      family = "binomial")
+logistic_model
+
+# Summary
+summary(logistic_model)
+
+predict_reg <- predict(logistic_model,
+                       test_reg, type = "response")
+predict_reg
+
+
+# Changing probabilities for prediction
+predict_reg <- ifelse(predict_reg >0.5, 1, 0)
+
+# Keep probabilities for ROC curve
+predict1_reg <- predict(logistic_model, test_reg, type = "response")
+
+
+# Evaluating model accuracy using confusion matrix
+table(test_reg$Attrition, predict_reg)
+
+missing_classerr <- mean(predict_reg != test_reg$Attrition)
+print(paste('Accuracy =', 1 - missing_classerr))
+
+# ROC-AUC Curve
+ROCPred <- prediction(predict1_reg, test_reg$Attrition)
+ROCPer <- performance(ROCPred, measure = "tpr",
+                      x.measure = "fpr")
+
+auc <- performance(ROCPred, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+
+# Plotting curve
+plot(ROCPer)
+plot(ROCPer, colorize = TRUE,
+     print.cutoffs.at = seq(0.1, by = 0.1),
+     main = "ROC CURVE")
+abline(a = 0, b = 1)
+
+auc <- round(auc, 4)
+legend(.6, .4, auc, title = "AUC", cex = 1)
+
+
+
